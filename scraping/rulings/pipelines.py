aade095efcf88d82f34acde23be061583dfbcc57
hubi <nicolas.hubacher@outlook.com>
2016-11-22 18:38:31 +0100

    save items in JSON file

--- scraping/rulings/pipelines.py
+++ scraping/rulings/pipelines.py
@@ -5,7 +5,22 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
+
 
 class RulingsPipeline(object):
+
+    def open_spider(self, spider):
+        # open data file when spider is started (make sure that encoding is correct)
+        self.file = open('../data/rulings.json', 'w+', encoding='utf-8')
+
+    def close_spider(self, spider):
+        # close data file when spider is done
+        self.file.close()
+
     def process_item(self, item, spider):
+        # write item to datafile
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(line)
         return item
+
--- scraping/run_scrapy.py
+++ scraping/run_scrapy.py
@@ -2,14 +2,19 @@ from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scraping.rulings import RulingSpider
+from scrapy.utils.project import get_project_settings
 
 
 def scrape_rulings():
-    # set the format of the logging messages
-    configure_logging({'LOG_FORMAT': '%(levelname)s | %(message)s'})
-    runner = CrawlerRunner()
+
+    # set the format of the logging messages and add pipeline(s)
+    settings = get_project_settings()
+    settings.set('LOG_FORMAT', '%(levelname)s | %(message)s')
+    settings.set('ITEM_PIPELINES', {'rulings.pipelines.RulingsPipeline': '100'})
+    configure_logging()
 
     # start the crawler
+    runner = CrawlerRunner(settings)
     d = runner.crawl(RulingSpider)
 
     # add callback that stops the reactor after RulingSpider has finished running
