9e3e02f446d961bd52d748f2a5a69427804ec8db
hubi <nicolas.hubacher@outlook.com>
2016-11-30 23:29:22 +0100

    basic stats about keywords and language

--- analysis/mongo.py
+++ analysis/mongo.py
@@ -33,4 +33,28 @@ for keyword in result:
 print(len(international_treaties))
 
 
+count_containing_field = lambda keyword_type: collection.find({keyword_type: {'$exists': 1}}).count()
+count_language = lambda language: collection.find({'language': language}).count()
+
+print(count_containing_field('international_treaties'))
+print(count_containing_field('international_customary_law'))
+print(count_containing_field('international_law_in_general'))
+print('===============================================')
+
+print('Language:')
+print(count_containing_field('language'))
+print(count_language('de'))
+print(count_language('fr'))
+print(count_language('it'))
+print(count_language('rr'))
+print('===============================================')
+
+result = collection.find({'ruling_id.bge_nb': 95, 'ruling_id.volume': 'III', 'ruling_id.ruling_nb': 76},
+                         {'title_of_judgement': 1, 'date': 1, 'type_of_proceeding': 1, 'language': 1, 'department': 1,
+                          'dossier_number': 1, 'involved_parties': 1, 'url': 1})
+print(result.count())
+for i, r in enumerate(result):
+    pprint(r, width=300)
+    if i > 1:
+        break
 client.close()
