72af8685f9938f0f38a4da5c8299104e0dd4d869
hubi <nicolas.hubacher@outlook.com>
2016-11-30 21:28:40 +0100

    querying all keywords relating to international treaties.

--- analysis/mongo.py
+++ analysis/mongo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pymongo
+from pprint import pprint
 
 
 mongo_uri = 'mongodb://localhost:27017'
@@ -12,5 +13,24 @@ db = client[database_name]
 collection = db[collection_name]
 
 print(collection.count())
+
+result = collection.aggregate([
+    {'$project': {'kw': '$international_treaties.keywords.keyword'}},
+    {'$unwind': '$kw'},
+    {'$group': {
+        '_id': "$kw",
+        'count': {'$sum': 1}}
+    },
+    {'$sort': {'_id': 1}}
+])
+
+international_treaties = []
+
+for keyword in result:
+    international_treaties.append(keyword)
+    print('%-50s | %3d' % (keyword['_id'], keyword['count']))
+
+print(len(international_treaties))
+
 
 client.close()
