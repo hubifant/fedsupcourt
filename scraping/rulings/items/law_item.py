29c6157bcf5ed5f62ca8aeaabb7752abd2729af9
hubi <nicolas.hubacher@outlook.com>
2016-12-06 21:14:55 +0100

    Extracting category name

--- scraping/rulings/items/law_item.py
+++ scraping/rulings/items/law_item.py
@@ -7,18 +7,12 @@
 
 from scrapy import Item, Field
 from scrapy.loader.processors import MapCompose, TakeFirst
-from abc import ABC, abstractmethod
+import re
 
 
-def _create_url(raw_url):
-    return 'https://www.admin.ch/' + raw_url
-
-
-class _HierarchicalIndex(Item, ABC):
-    @abstractmethod
-    def level_raw(self):
-        pass
-    # level = Field()
+def _extract_subcat_name(raw_name):
+    raw_name = re.sub(r'^.*?- ', '', raw_name, flags=re.DOTALL)
+    return re.sub(r'[^\w]*$', '', raw_name, flags=re.DOTALL)
 
 
 class CategoryItem(Item):
@@ -29,10 +23,13 @@ class CategoryItem(Item):
         output_processor=TakeFirst()
     )
     name = Field(
+        input_processor=MapCompose(_extract_subcat_name),
         output_processor=TakeFirst()
     )
     parent = Field(
         output_processor=TakeFirst()
     )
     children = Field()
-    url = Field()
+    url = Field(
+        output_processor=TakeFirst()
+    )
