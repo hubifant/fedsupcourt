bc136a94bc8ed3d1bf74b787286a4d4a77e7066c
hubi <nicolas.hubacher@outlook.com>
2016-12-12 01:07:22 +0100

    Query grouping kw counts per year

--- analysis/mongo.py
+++ analysis/mongo.py
@@ -12,10 +12,8 @@ client = pymongo.MongoClient(mongo_uri)
 db = client[database_name]
 collection = db[collection_name]
 
-print(collection.count())
 
-
-def save_keyword_list(keyword_type, path='.'):
+def save_keyword_list(keyword_type, path='.', verbose=False):
     file_path = path + '/' + keyword_type + '.csv'
     result = collection.aggregate([
         {'$project': {'kw': '$' + keyword_type + '.keywords.keyword'}},
@@ -41,38 +39,40 @@ def save_keyword_list(keyword_type, path='.'):
         for row in result:
             keyword_list.append(row)
             keyword_writer.writerow({"Keyword": row['_id'], "Occurrences": row["occurrences"]})
-            # print('%-50s | %3d' % (row['_id'], row['occurrences']))
+
+            if verbose:
+                print('%-50s | %3d' % (row['_id'], row['occurrences']))
 
     return keyword_list
 
 international_treaties = save_keyword_list('international_treaties')
 save_keyword_list('international_law_in_general')
 save_keyword_list('international_customary_law')
-print(len(international_treaties))
+# print(len(international_treaties))
 
 
 count_containing_field = lambda keyword_type: collection.find({keyword_type: {'$exists': 1}}).count()
 count_language = lambda language: collection.find({'language': language}).count()
 
-print(count_containing_field('international_treaties'))
-print(count_containing_field('international_customary_law'))
-print(count_containing_field('international_law_in_general'))
-print('===============================================')
+print('Keyword Counts: ')
+print('Total Number of Rulings:      %5d' % collection.count())
+print('International Treaties:       %5d' % count_containing_field('international_treaties'))
+print('International Customary Law:  %5d' % count_containing_field('international_customary_law'))
+print('International Law in General: %5d' % count_containing_field('international_law_in_general'))
+print('===================================')
 
 print('Language:')
-print(count_containing_field('language'))
-print(count_language('de'))
-print(count_language('fr'))
-print(count_language('it'))
-print(count_language('rr'))
-print('===============================================')
+print('Total Number of Rulings:                   %5d' % collection.count())
+print('Number of Rulings with extracted Language: %5d' % count_containing_field('language'))
+print('German:                                    %5d' % count_language('de'))
+print('French:                                    %5d' % count_language('fr'))
+print('Italian:                                   %5d' % count_language('it'))
+print('Rhaeto-Romance:                            %5d' % count_language('rr'))
+print('================================================')
 
 
-print('Department                                                                       | Count')
-print('---------------------------------------------------------------------------------+------')
 
-
-def save_departments(path='.'):
+def save_departments(path='.', verbose=False):
     departments = collection.aggregate([
         {'$group': {
             '_id': "$department",
@@ -89,16 +89,57 @@ def save_departments(path='.'):
                                            fieldnames=["Department", "Occurrences"])
         department_writer.writeheader()
 
+        if verbose:
+            print('Department                                                                       | Count')
+            print('---------------------------------------------------------------------------------+------')
+
         for dep in departments:
             if dep['_id'] is None:
                 dep['_id'] = 'Department not extractable'
             department_writer.writerow({'Department': dep['_id'], 'Occurrences': dep['count']})
             department_list.append(dep)
-            print('%-80s | %5d' % (dep['_id'], dep['count']))
+            if verbose:
+                print('%-80s | %5d' % (dep['_id'], dep['count']))
 
     return department_list
 
-save_departments()
+# save_departments()
+
+
+def count_by_year(keyword_type, path='.', verbose=False):
+    file_path = path + '/' + keyword_type + '_by_year' + '.csv'
+    count_variable = 'rulings_applying_' + keyword_type
+    result = collection.aggregate([
+        {'$match': {keyword_type: {'$exists': 1}}},
+        {
+            '$group': {
+                '_id': '$ruling_id.bge_nb',
+                count_variable: {
+                    '$sum': 1
+                }
+            }
+        },
+        {'$sort': {'_id': 1}}
+    ])
+
+    year_list = []
+    with open(file_path, 'w') as csv_file:
+        keyword_writer = csv.DictWriter(csv_file, quoting=csv.QUOTE_NONNUMERIC, fieldnames=["Keyword", count_variable])
+        keyword_writer.writeheader()
+
+        for row in result:
+            year_list.append(row)
+            keyword_writer.writerow({"Keyword": row['_id'], count_variable: row[count_variable]})
+
+            if verbose:
+                print('%-50s | %3d' % (row['_id'], row[count_variable]))
+
+    return year_list
+
+count_by_year('international_customary_law')
+count_by_year('international_law_in_general')
+count_by_year('extracted_laws')
+
 
 
 # result = collection.find({'ruling_id.bge_nb': 95, 'ruling_id.volume': 'III', 'ruling_id.ruling_nb': 76},
