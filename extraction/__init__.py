40a96ff70b726b25c9c067f6651d49883c1c35e7
hubi <nicolas.hubacher@outlook.com>
2017-01-06 00:53:21 +0100

    Moved code for extracting metadata to /extraction. Improved party and language extraction.

--- extraction/__init__.py
+++ extraction/__init__.py
@@ -1,3 +1,4 @@
 from .sr_number_extraction import SRNumberExtractor
 from .keyword_extraction import InternationalTreatyExtractor, InternationalCustomaryLawExtractor, \
     GeneralInternationalLawExtractor
+from .metadata_extraction import MetadataExtractor
